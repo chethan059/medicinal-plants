20c0490bc302ef38b2f5f83b7cfb20e54033b896
Likhitha123-abc <reddyallulikhitha@gmail.com>
2025-03-16 13:30:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+<script>
+      document.getElementById('go').addEventListener('click', function() {
+        const inputValue = document.getElementById("searchInput").value;
+              if (inputValue === 'Andhra Pradesh') {
+                window.location.href = 'andhrapradesh.html'
+              }else if(inputValue === 'Arunachal Pradesh'){
+                window.location.href = 'arunchalpradesh.html'
+              }else if(inputValue === 'Assam'){
+                window.location.href = 'assam.html'
+              }else if(inputValue === 'Bihar'){
+                window.location.href = 'bihar.html'
+              }else if(inputValue === 'Chandigarh'){
+                window.location.href = 'chandigarh.html'
+              }else if(inputValue === 'Chhattisgarh'){
+                window.location.href = 'chhattisgarh.html'
+              }else if(inputValue === 'Delhi'){
+                window.location.href = 'delhi.html'
+              }else if(inputValue === 'Goa'){
+                window.location.href = 'goa.html'
+              }else if(inputValue === 'Gujarat'){
+                window.location.href = 'gujarat.html'
+              }else if(inputValue === 'Haryana'){
+                window.location.href = 'haryana.html'
+              }else if(inputValue === 'Himachal Pradesh'){
+                window.location.href = 'himachalpradesh.html'
+              }else if(inputValue === 'Jammu and Kashmir'){
+                window.location.href = 'jammuandkashmir.html'
+              }else if(inputValue === 'Jharkhand'){
+                window.location.href = 'jharkhand.html'
+              }else if(inputValue === 'Karnataka'){
+                window.location.href = 'karnataka.html';
+              }else if(inputValue === 'Kerala'){
+                window.location.href = 'kerala.html';
+              }else if(inputValue === 'Madhya Pradesh'){
+                window.location.href = 'madhyapradesh.html';
+              }else if(inputValue === 'Maharashtra'){
+                window.location.href = 'maharashtra';
+              }else if(inputValue === 'Manipur'){
+                window.location.href = 'manipur.html';
+              }else if(inputValue === 'Meghalaya'){
+                window.location.href = 'meghalaya.html';
+              }else if(inputValue === 'Mizoram'){
+                window.location.href = 'mizoram.html';
+              }else if(inputValue === 'Nagaland'){
+                window.location.href = 'nagaland.html';
+              }else if(inputValue === 'Odisha'){
+                window.location.href = 'odisha.html';
+              }else if(inputValue === 'Punjab'){
+                window.location.href = 'punjab.html';
+              }else if(inputValue === 'Rajasthan'){
+                window.location.href = 'rajasthan.html';
+              }else if(inputValue === 'Sikkim'){
+                window.location.href = 'sikkim.html';
+              }else if(inputValue === 'Tamil Nadu'){
+                window.location.href = 'tamilnadu.html';
+              }else if(inputValue === 'Telangana'){
+                window.location.href = 'telangana.html';
+              }else if(inputValue === 'Tripura'){
+                window.location.href = 'tripura.html';
+              }else if(inputValue === 'Uttar Pradesh'){
+                window.location.href = 'uttarpradesh.html';
+              }else if(inputValue === 'Uttarakhand'){
+                window.location.href = 'uttarakhand.html';
+              }else if(inputValue === 'West Bengal'){
+                window.location.href = 'westbengal.html'
+              }
+      });
+      </script>
